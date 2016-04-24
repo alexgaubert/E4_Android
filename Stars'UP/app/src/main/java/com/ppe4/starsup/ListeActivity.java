8783692033f2184d3e613d6e68f07a2c33e63473
Mathieu Rouzé <mathieu.rze@gmail.com>
2016-04-25 01:28:55 +0200

    Parser et ListeActivity

--- Stars'UP/app/src/main/java/com/ppe4/starsup/ListeActivity.java
+++ Stars'UP/app/src/main/java/com/ppe4/starsup/ListeActivity.java
@@ -2,13 +2,28 @@ package com.ppe4.starsup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ListView;
 
 public class ListeActivity extends AppCompatActivity {
-
+    String url="http://10.0.2.2/android/players.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_liste);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ListView lv= (ListView) findViewById(R.id.lv);
+        final Downloader d=new Downloader(this,url,lv);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //EXECUTE DOWNLOAD
+                d.execute();
+            }
+        });
     }
 
 
